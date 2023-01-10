cab43763be41aa2df869084241cb55793a13cfdc
Charlotte-ER <68346113+Charlotte-ER@users.noreply.github.com>
2023-01-10 07:28:06 +0000

    Add function to create new annual subfolders for the new year.

---
+++ annual-folders.ps1
@@ -0,0 +1,35 @@
+
+Function Create-AnnualSubfolder($Path)
+{
+    # Get the current calendar year
+    [string] $currentYear = Get-Date -f yyyy
+
+    # Get last calendar year
+    [string] $lastYear = $currentYear-1
+
+    # Loop through all of the folders under TestFileStructure
+    Get-ChildItem  -Path $target -Recurse -Directory |
+
+    # Find folders relating to last year
+    Where-Object {$_.Name -eq $lastYear} | ForEach-Object {
+       
+        # Check whether the folder is empty
+        If ($_.getfiles().count -ne 0)
+        {
+            # If not empty, construct the file path of corresponding folder for the current year
+            $parentFolder = Split-Path -Path $_.FullName
+            $currentYearFolder = Join-Path -Path $parentFolder -ChildPath $currentYear
+            
+            # Check if the annual subfolder already exists
+            If (-not (Test-Path -Path $currentYearFolder))
+            {
+                # If it doesn't already exist, create the new annual folder
+                New-Item -ItemType directory -Path $currentYearFolder
+            }
+        }
+    }
+}
+
+$target = "your\file\path\here" # For Testing
+
+Create-AnnualSubfolder -Path $target
